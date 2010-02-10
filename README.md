e76f4f31eec60419255aac8e6a8320eb008c79a3
Wally Glutton <stungeye@gmail.com>
2010-02-09 19:18:04 -0600

    Created a walk-hugger with some randomness and a check for moves that lead to immediate death.

---
+++ README.md
@@ -0,0 +1,1 @@
+Playing with [the Google AI Challenge 2010](http://csclub.uwaterloo.ca/contest).
--- map.rb
+++ map.rb
@@ -8,125 +8,149 @@
 
 class Map
 
-	attr_reader :width, :height, :my_position, :opponent_position
-	
-	def initialize()
-	
-		@width = -1
-		@height = -1
-		@walls = []
-		@my_position = [-1,-1]
-		@opponent_position = [-1,-1]
-		
-		read_map
-		
-	end	
-	
-	def read_map
-	
-		begin
-		
-			#read the width and height from the first line
-			firstline = $stdin.readline("\n")
-			width, height = firstline.split(" ")
-			@width = width.to_i
-			@height = height.to_i
-			
-			#check for properly formatted width, height
-			if height == 0 or width == 0
-				p "OOPS!: invalid map dimensions: " + firstline
-				exit(1)
-			end
-			
-			#read the representation of the board
-			lines = []
-			@height.times do
-				lines += [$stdin.readline("\n").strip]
-			end
-			board = lines.join("")
-			
-			#get the wall data
-			@walls = board.split(//).map{|char| char == " " ? false : true}
-			
-			#get player starting locations
-			p1start = board.index("1").to_i
-			p2start = board.index("2").to_i
-			
-			if board.split(//).select{|char| char == "1"}.size > 1
-				p "OOPS!: found more than 1 location for player 1"
-				exit(1)
-			end
-			
-			if board.split(//).select{|char| char == "2"}.size > 1
-				p "OOPS!: found more than 1 location for player 2"
-				exit(1)
-			end
-			
-			p "OOPS!: Cannot find locations." if p1start == nil or p2start == nil
-			
-			pstartx = p1start % @width
-			pstarty = (p1start / @width)
-			@my_position = [pstartx, pstarty]
-			
-			pstartx = p2start % @width
-			pstarty = (p2start / @width)
-			@opponent_position = [pstartx, pstarty]
-					
-			
-		rescue EOFError => e
-			# Got EOF: tournament is finished.
-			exit(0)
-			
-		rescue => e
-			p  e
-			exit(1)
-		end
-	
-	end
-	private :read_map
-	
-	def each(&proc)
-		
-		(0..@height-1).each{|y|
-			(0..@width-1).each{|x|
-				proc[x, y, wall?(x, y)]
-			}
-		}
-		
-	end
-	
-	
-	def wall? (x, y)
-		return true if x < 0 or y < 0 or x >= @width or y >= @height
-		return @walls[x+@width*y]
-	end
-	
-	def to_string()
+  InvalidMove = Class.new(StandardError)
+  attr_reader :width, :height, :my_position, :opponent_position
+  DIRECTIONS = [:NORTH, :SOUTH, :EAST, :WEST]
+  
+  def initialize()
+  
+      @width = -1
+      @height = -1
+      @walls = []
+      @my_position = [-1,-1]
+      @opponent_position = [-1,-1]
+      
+      
+      read_map
+      
+  end	
+  
+  def read_map
+  
+      begin
+      
+          #read the width and height from the first line
+          firstline = $stdin.readline("\n")
+          width, height = firstline.split(" ")
+          @width = width.to_i
+          @height = height.to_i
+          
+          #check for properly formatted width, height
+          if height == 0 or width == 0
+              p "OOPS!: invalid map dimensions: " + firstline
+              exit(1)
+          end
+          
+          #read the representation of the board
+          lines = []
+          @height.times do
+              lines += [$stdin.readline("\n").strip]
+          end
+          board = lines.join("")
+          
+          #get the wall data
+          @walls = board.split(//).map{|char| char == " " ? false : true}
+          
+          #get player starting locations
+          p1start = board.index("1").to_i
+          p2start = board.index("2").to_i
+          
+          if board.split(//).select{|char| char == "1"}.size > 1
+              p "OOPS!: found more than 1 location for player 1"
+              exit(1)
+          end
+          
+          if board.split(//).select{|char| char == "2"}.size > 1
+              p "OOPS!: found more than 1 location for player 2"
+              exit(1)
+          end
+          
+          p "OOPS!: Cannot find locations." if p1start == nil or p2start == nil
+          
+          pstartx = p1start % @width
+          pstarty = (p1start / @width)
+          @my_position = [pstartx, pstarty]
+          
+          pstartx = p2start % @width
+          pstarty = (p2start / @width)
+          @opponent_position = [pstartx, pstarty]
+                  
+          
+      rescue EOFError => e
+          # Got EOF: tournament is finished.
+          exit(0)
+          
+      rescue => e
+          p  e
+          exit(1)
+      end
+  
+  end
+  private :read_map
+  
+  def each(&proc)
+      
+      (0..@height-1).each{|y|
+          (0..@width-1).each{|x|
+              proc[x, y, wall?([x, y])]
+          }
+      }
+      
+  end
+  
+  
+  def wall? (coordinates)
+      x, y = coordinates
+      return true if x < 0 or y < 0 or x >= @width or y >= @height
+      return @walls[x+@width*y]
+  end
+  
+  def to_string()
 
-		out = ""
-		counter = 0
-				
-		@height.times do
-			@width.times do
-				out += @walls[counter] == true ? "#" : "-"
-				counter+=1
-			end
-			out += "\n"
-		end
-		
-		
-		return out
-		
-	end
+      out = ""
+      counter = 0
+              
+      @height.times do
+          @width.times do
+              out += @walls[counter] == true ? "#" : "-"
+              counter+=1
+          end
+          out += "\n"
+      end
+      
+      
+      return out
+      
+  end
 
-
-
-	def make_move(direction)
+  def make_move(direction)
+  
+      $stdout << ({:NORTH=>1, :SOUTH=>3, :EAST=>2, :WEST=>4}[direction])
+      $stdout << "\n"
+      $stdout.flush
+      
+  end
 	
-		$stdout << ({:NORTH=>1, :SOUTH=>3, :EAST=>2, :WEST=>4}[direction])
-		$stdout << "\n"
-		$stdout.flush
-		
-	end
-	
+  def rel(direction, origin = nil)
+    origin = @my_position if origin.nil?
+    x, y   = origin
+    
+    case direction
+      when :NORTH
+        [y - 1, x]
+      when :SOUTH
+        [y + 1, x]
+      when :EAST
+        [y, x + 1]
+      when :WEST
+        [y, x - 1]
+      else
+        raise InvalidMove;
+    end
+  end
+  
+  def adjacent(origin)
+    DIRECTIONS.inject([]) { |adjacents, direction| adjacents << rel(direction, origin) }
+  end
+  
 end
